b808acb8c77a42a1cda61aca5a4ec3ff88208873
Abhishek kanojia <abhishekka3193@gmail.com>
2017-11-28 23:51:36 +0530

    Fixes and refactoring

--- exercises/js/inputHint.js
+++ exercises/js/inputHint.js
@@ -1,57 +1,58 @@
-function CreateInputHint(form) {
-  this.form = form;
-  this.label = this.form.find('label');
-  this.inputField = this.form.find('input[name=q]');
+function InputHint(data) {
+  this.form = data.form;
+  this.label = data.label;
+  this.inputField = data.inputField;
 }
 
-CreateInputHint.prototype.ClassName = 'hint';
+InputHint.prototype.ClassName = 'hint';
 
-CreateInputHint.prototype.changeValueOfInput = function() {
-  this.inputField.val(this.label.text());
-};
-
-CreateInputHint.prototype.removeLabel = function() {
+InputHint.prototype.removeLabel = function() {
   this.addHint();
   this.label.remove();
 };
 
-CreateInputHint.prototype.showHint = function(hintText) {
+InputHint.prototype.addHintClass = function(hintText) {
   var _this = this;
   return function(){
-    if(_this.inputField.val().length === 0) {
+    if(_this.inputField.val().trim().length === 0) {
       _this.inputField.val(hintText);
       _this.addClassToField();
     }
   };
 };
 
-CreateInputHint.prototype.addClassToField = function() {
+InputHint.prototype.addClassToField = function() {
   this.inputField.addClass(this.ClassName);
 };
 
-CreateInputHint.prototype.addHint = function() {
+InputHint.prototype.addHint = function() {
   this.inputField.val(this.label.text());
   this.addClassToField();
 };
 
-CreateInputHint.prototype.removeClassFromField = function() {
+InputHint.prototype.removeClassFromField = function() {
   var _this = this;
   return function(){
-    _this.inputField.val('');
-    _this.inputField.removeClass(_this.ClassName);
+    _this.inputField
+      .val('')
+      .removeClass(_this.ClassName);
   };
 };
 
-CreateInputHint.prototype.init = function() {
+InputHint.prototype.init = function() {
   var _this = this;
-  var labelText = this.label.text();
-  $(this.inputField).focus(_this.removeClassFromField());
-  $(this.inputField).blur(_this.showHint(labelText));
-  this.removeLabel();
+  $(this.inputField)
+    .focus(_this.removeClassFromField())
+    .blur(_this.addHintClass(_this.label.text()));
+  _this.removeLabel();
 };
 
 $(document).ready(function(){
-  var form = $("#main").find('form#search');
-  var searchHandler = new CreateInputHint(form);
+  var data = {
+    form: $("#main").find('form#search'),
+    label: $("form#search").find('label'),
+    inputField: $("form#search").find('input[name=q]')
+  };
+  var searchHandler = new InputHint(data);
   searchHandler.init();
 });
