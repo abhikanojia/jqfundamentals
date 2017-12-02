f11ab03afbfe38040e0d05960e02d8aced7e05bc
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-12-02 11:59:36 +0530

    Completed exercise 6.1

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,53 +1,42 @@
 function LoadContent(data) {
   this.headings = data.headings;
-  this.externalFile = data.externalFile;
-  this.failError = data.failError;
+  this.filePath = data.filePath;
+  this.attribute = data.attribute;
 }
 
-LoadContent.prototype.loadSection = function(sectionName) {
-  var request = $.ajax({
-    url: this.externalFile,
-    method: "GET"
-  });
 
-  request.success(function(data){
-    console.log('success')
-    console.log(data);
-  });
-
-  request.fail(function(){
-    alert(_this.failError);
-  });
-};
-
-LoadContent.prototype.getAttributeFromString = function(string) {
-  return string.substr(string.indexOf('#'));
-};
-
-LoadContent.prototype.addReference = function(element) {
+LoadContent.prototype.getTarget = function(element) {
   var href = $(element).find('a').attr('href');
-  console.log(element);
-  var dataValue = this.getAttributeFromString(href);
-  console.log(dataValue);
-  $(element).find('a').data('redirect',dataValue);
-  console.log('done')
+  return href.substr(href.indexOf('#'));
+};
+
+LoadContent.prototype.addReference = function() {
+  var _this = this;
+  this.headings.each(function(){
+    $('<div/>').insertAfter(this).attr(_this.attribute, _this.getTarget(this));
+  });
+};
+
+LoadContent.prototype.loadData = function(element) {
+  var container = $(element).next('div[data-target]');
+  $(container).load(this.filePath + " " + $(container).data('target'));
 };
 
 LoadContent.prototype.init = function() {
   var _this = this;
+  this.addReference();
   this.headings.bind('click', function(event){
     event.preventDefault();
-    _this.addReference(this)
+    _this.loadData(this);
   })
 };
 
 
 var data = {
-  externalFile: 'file:///media/abhishekkanojia/harddisk/exercise/jqfundamentals/data/blog.html',
-  failError: 'Something Went wrong.',
-  headings: $('#blog').find('h3')
+  filePath: 'data/blog.html',
+  headings: $('#blog').find('h3'),
+  attribute: 'data-target'
 };
 
 var loadContent = new LoadContent(data);
 loadContent.init();
-console.log(loadContent);
