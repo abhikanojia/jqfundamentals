4de0de972e6999411df7d836399920e91e4be7de
Abhishek kanojia <abhishekka3193@gmail.com>
2017-12-05 04:13:20 +0530

    Tab js fixes and refactor

--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -1,31 +1,28 @@
 function TabNaviation(data) {
-  this.ul = data.ul;
   this.className = data.className;
   this.divModules = data.divModules;
-  this.firstDivModule = data.firstDivModule;
+  this.firstDivModule = this.divModules.first();
 }
 
-TabNaviation.prototype.getDiv = function(id) {
-  return $(this.getId(id));
-};
-
-TabNaviation.prototype.getId = function(id) {
+TabNaviation.prototype.getSelector = function(id) {
   return "#".concat(id);
 };
 
 
-TabNaviation.prototype.createList = function(headings) {
+TabNaviation.prototype.createList = function() {
   var _this = this;
-  $.each(headings, function(){
-    $(_this.newListItem(this.valueOf()))
+  $.each(this.divModules, function(){
+    var heading = $(this).find('h2').text().toLowerCase();
+    $(_this.newListItem(heading))
       .appendTo(_this.ul)
       .bind('click', function(){
         var idString = $(this).text().toLowerCase();
+        var div = $(this).data('div');
 
         $(this).toggleClass(_this.className)
           .siblings()
           .removeClass(_this.className);
-        $(_this.getDiv(idString)).toggle()
+        $(_this.getSelector(div)).toggle()
           .siblings('.module')
           .hide();
       });
@@ -33,30 +30,21 @@ TabNaviation.prototype.createList = function(headings) {
 };
 
 TabNaviation.prototype.newListItem = function(content) {
-  return '<li>' + content + '</li>';
-};
-
-TabNaviation.prototype.getHeadings = function() {
-  var headings = [];
-  this.divModules.each(function(){
-    headings.push($(this).find('h2').text());
-  });
-  return headings;
+  var $li = $("<li/>");
+  return $li.attr('data-div', content).text(content);
 };
 
 TabNaviation.prototype.init = function() {
   this.divModules.hide();
-  this.ul = $(this.ul).insertBefore(this.firstDivModule);
-  this.createList(this.getHeadings());
+  this.ul = $('<ul/>').insertBefore(this.firstDivModule);
+  this.createList();
 };
 
 
 $(document).ready(function(){
   var data = {
-    ul: '<ul></ul>',
     className: 'current',
-    divModules: $('div.module'),
-    firstDivModule: $('div.module:first'),
+    divModules: $('div.module')
   };
   var tabs = new TabNaviation(data);
   tabs.init();
